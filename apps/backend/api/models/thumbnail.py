e522660b88e2046cf598abfe16664e3893bdfcdf
E-20-8-1-N <82087610+E-20-8-1-N@users.noreply.github.com>
2025-12-29 14:13:05 +0800

    Fix: Enhance error handling for thumbnail generation and related processes

--- apps/backend/api/models/thumbnail.py
+++ apps/backend/api/models/thumbnail.py
@@ -3,6 +3,8 @@ import os
 from django.conf import settings
 from django.db import models
 from PIL import Image
+from pillow_heif import register_heif_opener
+register_heif_opener() # Register HEIF opener for Pillow
 
 from api.models.photo import Photo
 from api.thumbnails import (
