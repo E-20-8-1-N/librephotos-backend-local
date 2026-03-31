d4db18f68a1e5423effd14efb4892816608655de
E-20-8-1-N <82087610+E-20-8-1-N@users.noreply.github.com>
2026-03-31 17:13:25 +0800

    feat: add backend endpoint to delete missing photos for a user

--- apps/backend/librephotos/urls.py
+++ apps/backend/librephotos/urls.py
@@ -262,6 +262,10 @@ urlpatterns = [
     re_path(r"^api/deletemissingphotos", views.DeleteMissingPhotosView.as_view()),
     re_path(r"^api/classifymedia", views.ClassifyMediaView.as_view()),
     re_path(r"^api/generateocr", views.GenerateOcrView.as_view()),
+    re_path(
+        r"^api/backenddeletemissingphotos",
+        views.BackendDeleteMissingPhotosView.as_view(),
+    ),
     re_path(r"^api/autoalbumgen", album_auto.AutoAlbumGenerateView.as_view()),
     re_path(r"^api/autoalbumtitlegen", album_auto.RegenerateAutoAlbumTitles.as_view()),
     # Photo Stacks - Organizational grouping (bursts, brackets, manual)
