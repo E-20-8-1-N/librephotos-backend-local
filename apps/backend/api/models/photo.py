58cd195668b9a949c85f5cd7626188d4248bdd63
E-20-8-1-N <82087610+E-20-8-1-N@users.noreply.github.com>
2026-04-10 18:30:36 +0800

    fix: add check for None album_date in Photo model and ensure caption is only updated if generated in PhotoSearch model

--- apps/backend/api/models/photo.py
+++ apps/backend/api/models/photo.py
@@ -467,6 +467,8 @@ class Photo(models.Model):
             return
 
         album_date = self._find_album_date()
+        if album_date is None:
+            return
         city_name = places[-2]
         if album_date.location and len(album_date.location) > 0:
             prev_value = album_date.location
